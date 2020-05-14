a1831241a2796b3f5b0f4971cd1c44f8f39f19cd
Md Yaseen Ahmed <60700271+mdyaseencs19@users.noreply.github.com>
2020-05-14 09:08:03 +0530

    Create 02 - Operators_and_Operands.py

---
+++ 02 - Operators_and_Operands.py
@@ -0,0 +1,18 @@
+print(100 + 200)        # 300
+print(100 - 200)        # -100
+print(10 * 20)          # 200
+print(10 / 3)           # 3.3333333333333335 
+print(10 // 3)          # 3 [Integer Division]
+print(10 % 3)           # 1
+print(4 ** 2)           # 4^2 = 16 [Exponentiation]
+print(4 + 10 / 2)       # 10 / 2 = 5.0 + 4 = 9.0
+print((4 + 10) / 2)     # 4 + 10 = 14 / 2 = 7.0
+
+"""
+Order of Evaluation of an Expression:
+    
+    1. Parenthesis
+    2. Exponentiation
+    3. Multiplication & Division
+    4. Addition & Subtraction
+"""
